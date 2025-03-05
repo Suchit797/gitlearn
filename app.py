68c0c005eca878fe73363b6cb9fab09ff5fa1e6a
suchit <suchitkumar985@gmail.com>
2025-03-05 19:10:55 +0530

    this is my 4 th commit

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 def myname(a) : 
     print(a)
     return a 
+
+myname('SUCHIT')
+myname('KABIR')
+
+def name(b) :
+    while True == True :
+        print(b)
+        return b 
+    
+name('SUCHIT')
